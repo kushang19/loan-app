7de1a7f2483140262aff07ec37889bd1f6f3fb0f
Kushang Vijay Gharat <kushang81@gmail.com>
2025-08-13 01:50:16 +0530

    base url changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:5000/api";
-const API_BASE_URL = "https://loan-app-backend-f3r1.onrender.com";
+const API_BASE_URL = "https://loan-app-backend-f3r1.onrender.com/api";
 
 // --------------------
 // POST - Save Data
